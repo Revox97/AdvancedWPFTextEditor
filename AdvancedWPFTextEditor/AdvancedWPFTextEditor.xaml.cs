81597a0f80e52040ca56b39967a7f4ebdc25a2d8
Leopold Pohlmann <Leopold.Pohlmann@gmx.net>
2024-10-28 14:41:17 +0100

    Rename UserControl1.cs to AdvancedWPFTextEditor.cs

--- AdvancedWPFTextEditor/AdvancedWPFTextEditor.xaml.cs
+++ AdvancedWPFTextEditor/AdvancedWPFTextEditor.xaml.cs
@@ -1,22 +1,13 @@
-using System.Text;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AdvancedWPFTextEditor
 {
     /// <summary>
     /// Interaction logic for UserControl1.xaml
     /// </summary>
-    public partial class UserControl1 : UserControl
+    public partial class AdvancedWPFTextEditor : UserControl
     {
-        public UserControl1()
+        public AdvancedWPFTextEditor()
         {
             InitializeComponent();
         }
